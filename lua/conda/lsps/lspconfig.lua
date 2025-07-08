c333ba9cac44652e8c315993da08ef512d39a295
sergius <UO293837@uniovi.es>
2025-07-08 02:00:18 +0200

    Fixed compatibility with Nvim 0.11 LSP API changes

--- lua/conda/lsps/lspconfig.lua
+++ lua/conda/lsps/lspconfig.lua
@@ -2,7 +2,8 @@ local M = {}
 
 ---@return nil
 M.restart_lsps = function()
-  vim.api.nvim_command("LspRestart")
+  vim.api.nvim_command("LspRestart pyright")
+  vim.api.nvim_command("LspRestart basedpyright")
 end
 
 return M
